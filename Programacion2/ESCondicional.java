25dcb99912b826f19795741970153d8baaa4210a
jean_franco <82102346+jean-franco660@users.noreply.github.com>
2021-04-28 10:46:11 -0500

    Delete Programacion2 directory

--- Programacion2/ESCondicional.java
+++
@@ -1,4 +0,0 @@
-import java.util.Scanner
-class EstCondicional{
-   static Scanner teclado=new Scanner(System.in);
-  
--- Programacion2/index.py
+++
@@ -1,7 +0,0 @@
-print("Hola_jean-franco")
-a=45
-b=5
-print("Suma",a+b)
-print("resta",a-b)
-print("Division".a/b)
-print("Multiplicacion",a*b)
